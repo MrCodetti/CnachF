a7c6df8ae7a13baacb493164ded70dbe075d7ea5
Bartosz Porada <bporada@outlook.com>
2020-04-21 11:09:12 +0200

    Projektdateien hinzufügen.

---
+++ CnachF/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tempBerechnung;
+
+namespace CnachF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wahl = 0;
+            int celsius, fahrenheit;
+
+            Console.WriteLine("Auswahl Konvertierungstyp");
+            Console.WriteLine("1. Celsius --> Fahrenheit");
+            Console.WriteLine("2. Fahrenheit --> Celsius");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out wahl))
+            {
+                switch (wahl)
+                {
+                    case 1:
+                        Console.WriteLine("Celsius wert: ");
+                        celsius = int.Parse(Console.ReadLine());
+                        Console.WriteLine(Berechnung.CnachF(celsius));
+                        break;
+                    case 2:
+                        Console.WriteLine("Fahrenheit wert: ");
+                        fahrenheit = int.Parse(Console.ReadLine());
+                        Console.WriteLine(Berechnung.FnachC(fahrenheit));
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Syntax Error");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ tempBerechnung/Berechnung.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace tempBerechnung
+{
+    public class Berechnung
+    {
+        public static double CnachF(int grad)
+        {
+            return grad * 1.8 + 32;
+        }
+
+        public static double FnachC(int grad)
+        {
+            return (grad - 32)/1.8;
+        }
+    }
+}
